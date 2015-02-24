3cb55ae3bc578ea4c27d0f1431746029a36e395e
Jollen Chen <jollen@moko365.com>
2015-02-24 13:12:23 +0800

    add: local images for publishing

---
+++ images/ftp.sh
@@ -0,0 +1,20 @@
+#sleep 1; curl -o xWO0DEd.jpg http://i.imgur.com/xWO0DEd.jpg
+#sleep 1; curl -o 5vpCFjM.png http://i.imgur.com/5vpCFjM.png
+#sleep 1; curl -o 5iD2ehn.jpg http://i.imgur.com/5iD2ehn.jpg
+#sleep 1; curl -o 2Nx0FSh.jpg http://i.imgur.com/2Nx0FSh.jpg
+#sleep 1; curl -o GzFrp2W.jpg http://i.imgur.com/GzFrp2W.jpg
+#sleep 1; curl -o mJZWmMH.jpg http://i.imgur.com/mJZWmMH.jpg
+#sleep 1; curl -o aszlXqG.jpg http://i.imgur.com/aszlXqG.jpg
+#sleep 1; curl -o 3Duw0rK.jpg http://i.imgur.com/3Duw0rK.jpg
+#sleep 1; curl -o Iom5KME.jpg http://i.imgur.com/Iom5KME.jpg
+#ftp http://i.imgur.com/2Nx0FSh.jpg
+#ftp http://i.imgur.com/LT0xdWQ.jpg
+#ftp http://i.imgur.com/99qiOYr.jpg
+#ftp http://i.imgur.com/GV3hZjz.jpg
+#ftp http://i.imgur.com/zo38TF0.jpg
+#ftp http://i.imgur.com/h6il6ET.png
+#ftp http://i.imgur.com/kxaUJKu.jpg
+#ftp http://i.imgur.com/uhJPzho.jpg
+ftp http://i.imgur.com/AKgq0Qf.jpg
+ftp http://i.imgur.com/RGTk67m.png
+ftp http://i.imgur.com/BFturz1.png
